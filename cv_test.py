886bbe4fbd2f523f8263f8898075e16776e5cb7d
siddarthm272 <43051084+siddarthm272@users.noreply.github.com>
2019-10-17 20:42:33 -0700

    passed a shorter video

--- cv_test.py
+++ cv_test.py
@@ -8,6 +8,7 @@ import traceback
 import os
 from matplotlib import pyplot as plt
 
+vid = cv2.VideoCapture('video.mp4')
 hsv_map = np.zeros((180, 256, 3), np.uint8)
 h, s = np.indices(hsv_map.shape[:2])
 hsv_map[:,:,0] = h
@@ -17,21 +18,12 @@ hsv_map = cv2.cvtColor(hsv_map, cv2.COLOR_HSV2BGR)
 hist_scale = 10
 
 while(True):
-<<<<<<< HEAD
-    frame = cv2.imread('test_photos/20degrees_18inches.png')
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    low_green = np.array([62,181,93])
-    high_green= np.array([88,255,174])
-
-    mask = cv2.inRange(hsv, low_green, high_green)
-    _, contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-=======
-    
-    
 
     #start of cv code
-    frame = cv2.imread('test_photos/0degrees_18inches.png')
- 
+    # frame = cv2.imread('test_photos/0degrees_18inches.png')
+
+    _, frame = vid.read()
+
     # cv2.imshow('camera', frame)
 
     small = cv2.pyrDown(frame)
@@ -50,27 +42,27 @@ while(True):
 
     vis = hsv_map*h[:,:,np.newaxis] / 255.0
 
-    
+
     h_vals = vis[:,:, 0]
     print(255*h_vals[h_vals != 0])
     h_vals = 255*h_vals[h_vals != 0]
-    hue_hist=plt.hist(h_vals,180,[0,180]);
+    hue_hist=plt.hist(h_vals,180,[0,180])
     print("mean: ", h_vals.mean())
     print("low: ", h_vals.mean() - (0.3*h_vals.std()))
     print("high: ", h_vals.mean() + (0.1*h_vals.std()))
-    
+
 
 
 
     # low_green = np.array([62,181,93])
 	# high_green= np.array([88,255,174])
-    
+
     low_green = np.array([int(h_vals.mean() - (0.3*h_vals.std())), 181,0])
     high_green= np.array([int(h_vals.mean() + (0.1*h_vals.std())),255,255])
-    
+
     mask = cv2.inRange(hsv_2, low_green, high_green)
     contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
->>>>>>> 8f9734790ca043efae65a6e8472ca5ded2ed76b0
+
     #sorts contours by area
     #contours.sort(key = cv2.contourArea, reverse = True)
 
@@ -108,21 +100,15 @@ while(True):
         cv2.drawContours(frame, [rotated_rect.box], 0, (0, 0, 255), 2)
         cv2.imshow("Contours", mask)
         cv2.imshow("Frame", frame)
-        cv2.waitKey(3)
+        cv2.waitKey(1)
 
     # if there are less than two rectangles, return -99, -1
     if (len(rotated_boxes) < 2):
         cv2.imshow("Contours", mask)
         cv2.imshow("Frame", frame)
-        cv2.waitKey(3)
+        cv2.waitKey(1)
         #return -99, -1
 
     cv2.imshow("Contours", mask)
     cv2.imshow("Frame", frame)
-<<<<<<< HEAD
-    cv2.waitKey(3)
-=======
-
-    cv2.waitKey(3)
-
->>>>>>> 8f9734790ca043efae65a6e8472ca5ded2ed76b0
+    # cv2.waitKey(1)

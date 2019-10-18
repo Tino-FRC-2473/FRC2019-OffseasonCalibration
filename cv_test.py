8f9734790ca043efae65a6e8472ca5ded2ed76b0
pguduguntla <praneethg347@gmail.com>
2019-10-17 20:22:06 -0700

    working calib code

--- cv_test.py
+++ cv_test.py
@@ -6,15 +6,61 @@ import subprocess
 import imghdr
 import traceback
 import os
+from matplotlib import pyplot as plt
+
+hsv_map = np.zeros((180, 256, 3), np.uint8)
+h, s = np.indices(hsv_map.shape[:2])
+hsv_map[:,:,0] = h
+hsv_map[:,:,1] = s
+hsv_map[:,:,2] = 255
+hsv_map = cv2.cvtColor(hsv_map, cv2.COLOR_HSV2BGR)
+hist_scale = 10
 
 while(True):
-    frame = cv2.imread('test_photos/20degrees_18inches.png')
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    low_green = np.array([62,181,93])
-    high_green= np.array([88,255,174])
     
-    mask = cv2.inRange(hsv, low_green, high_green)
-    _, contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    
+
+    #start of cv code
+    frame = cv2.imread('test_photos/0degrees_18inches.png')
+ 
+    # cv2.imshow('camera', frame)
+
+    small = cv2.pyrDown(frame)
+
+    hsv_2 = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    hsv = cv2.cvtColor(small, cv2.COLOR_BGR2HSV)
+    # print(hsv[...,2])
+    dark = hsv[...,2] < 32
+    # print(dark)
+    hsv[dark] = 0
+    h = cv2.calcHist([hsv], [0, 1], None, [180, 256], [0, 180, 0, 256])
+
+    h = np.clip(h*0.005*hist_scale, 0, 1)
+
+
+    vis = hsv_map*h[:,:,np.newaxis] / 255.0
+
+    
+    h_vals = vis[:,:, 0]
+    print(255*h_vals[h_vals != 0])
+    h_vals = 255*h_vals[h_vals != 0]
+    hue_hist=plt.hist(h_vals,180,[0,180]);
+    print("mean: ", h_vals.mean())
+    print("low: ", h_vals.mean() - (0.3*h_vals.std()))
+    print("high: ", h_vals.mean() + (0.1*h_vals.std()))
+    
+
+
+
+    # low_green = np.array([62,181,93])
+	# high_green= np.array([88,255,174])
+    
+    low_green = np.array([int(h_vals.mean() - (0.3*h_vals.std())), 181,0])
+    high_green= np.array([int(h_vals.mean() + (0.1*h_vals.std())),255,255])
+    
+    mask = cv2.inRange(hsv_2, low_green, high_green)
+    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     #sorts contours by area
     #contours.sort(key = cv2.contourArea, reverse = True)
 
@@ -63,4 +109,6 @@ while(True):
 
     cv2.imshow("Contours", mask)
     cv2.imshow("Frame", frame)
+
     cv2.waitKey(3)
+

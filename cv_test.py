15e08d00ff44021c15706b4aaea1e66e3b567c9a
pguduguntla <praneethg347@gmail.com>
2019-11-12 18:28:01 -0800

    fixed hsv range issue

--- cv_test.py
+++ cv_test.py
@@ -9,7 +9,7 @@ from matplotlib import pyplot as plt
 import random
 import pdb
 
-vid = cv2.VideoCapture('video2.mp4')
+# vid = cv2.VideoCapture('video.mp4')
 
 hsv_map = np.zeros((180, 256, 3), np.uint8)
 h, s = np.indices(hsv_map.shape[:2])
@@ -20,7 +20,7 @@ hsv_map = cv2.cvtColor(hsv_map, cv2.COLOR_HSV2BGR)
 hist_scale = 10
 
 rgb_data = np.loadtxt('green_data.csv', dtype= np.uint8, delimiter=',')
-bgr_data = np.copy(rgb_data)
+bgr_data = np.copy(rgb_data) 
 bgr_data[:,0] = rgb_data[:,2]
 bgr_data[:,2] = rgb_data[:,0]
 
@@ -32,16 +32,15 @@ true_green_vals = cv2.cvtColor(bgr_data, cv2.COLOR_BGR2HSV)
 pixel_count=len(true_green_vals)
 
 
-low_green = np.array([69,152,0])
-high_green= np.array([86,219,255])
+low_green = np.array([68,100, 10])
+high_green= np.array([84,255,255])
 
-def get_new_hsv(res):]
-
+def get_new_hsv(res):
     global true_green_vals
     global low_green
     global high_green
-    
-    if(res is None):
+
+    if(len(res) == 0):
         return low_green, high_green
     
 
@@ -60,25 +59,21 @@ def get_new_hsv(res):]
     low_h, low_s, low_v = (h.mean() - 3.25*h.std()), (s.mean() - 3.5*s.std()), (v.mean() - 3.25*v.std())
     high_h, high_s, high_v = (h.mean() + 3.25*h.std()), (s.mean() + 3.5*s.std()), (v.mean() + 3.25*v.std())
 
-    return np.array([int(low_h), int(low_s), int(low_h)]), np.array([int(high_h), int(high_s), int(high_v)])
+    return np.array([int(low_h), int(low_s), int(low_v)]), np.array([int(high_h), int(high_s), int(high_v)])
     
 
 while(True):
-    # frame = cv2.imread('test_photos/0degrees_24inches.png')
+    frame = cv2.imread('test_photos/0degrees_36inches.png')
 
-    _, frame = vid.read()
+    # _, frame = vid.read()
 
-    # cv2.imshow('camera', frame)
 
-    small = cv2.pyrDown(frame)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    hsv_2 = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-    hsv = cv2.cvtColor(small, cv2.COLOR_BGR2HSV)
   
-    mask = cv2.inRange(hsv_2, low_green, high_green)
+    mask = cv2.inRange(hsv, low_green, high_green)
     
-    greens=hsv_2[np.where((mask==255))]
+    greens=hsv[np.where((mask==255))]
 
     low_green, high_green = get_new_hsv(greens)
 
@@ -87,5 +82,5 @@ while(True):
     
     cv2.imshow("Mask", mask)
     cv2.imshow("Frame", frame)
-    cv2.imshow("True greens", greens)
+    # cv2.imshow("True greens", greens)
     cv2.waitKey(1)
